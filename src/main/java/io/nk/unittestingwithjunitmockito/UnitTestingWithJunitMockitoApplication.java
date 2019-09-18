7122a660bbf6d7fd02dd1659d453531cc41da2e4
King <nigus.kidane@gmail.com>
2019-09-17 18:18:09 -0600

    Spring Boot Project Created

---
+++ src/main/java/io/nk/unittestingwithjunitmockito/UnitTestingWithJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package io.nk.unittestingwithjunitmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestingWithJunitMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestingWithJunitMockitoApplication.class, args);
+    }
+
+}
